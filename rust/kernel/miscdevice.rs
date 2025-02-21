0c80a7c9edbd208ec45d752be5c7c2959954a340
Tamir Duberstein <tamird@gmail.com>
2025-02-21 15:27:40 -0500

    rust: types: add `ForeignOwnable::PointedTo`
    Allow implementors to specify the foreign pointer type; this exposes
    information about the pointed-to type such as its alignment.
    
    This requires the trait to be `unsafe` since it is now possible for
    implementors to break soundness by returning a misaligned pointer.
    
    Encoding the pointer type in the trait (and avoiding pointer casts)
    allows the compiler to check that implementors return the correct
    pointer type. This is preferable to directly encoding the alignment in
    the trait using a constant as the compiler would be unable to check it.
    
    Acked-by: Danilo Krummrich <dakr@kernel.org>
    Signed-off-by: Tamir Duberstein <tamird@gmail.com>
    Reviewed-by: Andreas Hindborg <a.hindborg@kernel.org>

--- rust/kernel/miscdevice.rs
+++ rust/kernel/miscdevice.rs
@@ -284,7 +284,7 @@ unsafe extern "C" fn fops_open<T: MiscDevice>(
     // fops_* methods in this file, which all correctly cast the private data to the new type.
     //
     // SAFETY: The open call of a file can access the private data.
-    unsafe { (*raw_file).private_data = ptr.into_foreign() };
+    unsafe { (*raw_file).private_data = ptr.into_foreign().cast() };
 
     0
 }
@@ -298,7 +298,7 @@ unsafe extern "C" fn fops_release<T: MiscDevice>(
     file: *mut bindings::file,
 ) -> c_int {
     // SAFETY: The release call of a file owns the private data.
-    let private = unsafe { (*file).private_data };
+    let private = unsafe { (*file).private_data }.cast();
     // SAFETY: The release call of a file owns the private data.
     let ptr = unsafe { <T::Ptr as ForeignOwnable>::from_foreign(private) };
 
@@ -345,7 +345,7 @@ unsafe extern "C" fn fops_ioctl<T: MiscDevice>(
     arg: c_ulong,
 ) -> c_long {
     // SAFETY: The ioctl call of a file can access the private data.
-    let private = unsafe { (*file).private_data };
+    let private = unsafe { (*file).private_data }.cast();
     // SAFETY: Ioctl calls can borrow the private data of the file.
     let device = unsafe { <T::Ptr as ForeignOwnable>::borrow(private) };
 
@@ -370,7 +370,7 @@ unsafe extern "C" fn fops_compat_ioctl<T: MiscDevice>(
     arg: c_ulong,
 ) -> c_long {
     // SAFETY: The compat ioctl call of a file can access the private data.
-    let private = unsafe { (*file).private_data };
+    let private = unsafe { (*file).private_data }.cast();
     // SAFETY: Ioctl calls can borrow the private data of the file.
     let device = unsafe { <T::Ptr as ForeignOwnable>::borrow(private) };
 
@@ -394,7 +394,7 @@ unsafe extern "C" fn fops_show_fdinfo<T: MiscDevice>(
     file: *mut bindings::file,
 ) {
     // SAFETY: The release call of a file owns the private data.
-    let private = unsafe { (*file).private_data };
+    let private = unsafe { (*file).private_data }.cast();
     // SAFETY: Ioctl calls can borrow the private data of the file.
     let device = unsafe { <T::Ptr as ForeignOwnable>::borrow(private) };
     // SAFETY:
